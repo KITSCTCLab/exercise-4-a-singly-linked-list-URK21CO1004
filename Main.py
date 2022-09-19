bbd25f49b91d003e4b3ff5c02018f9b4e87ae610
URK21CO1004 <91770239+URK21CO1004@users.noreply.github.com>
2022-09-19 22:39:49 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,68 +1,79 @@
+from typing import Optional
+
 class Node:
-  """
-  Class implementing node of the LinkedList
-  Attributes:
-    -> data - data held by the node
-    -> next - link to the next node
-  """
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+
+    def __init__(self, data=None, next=None):
+        """
+        Initialises the Node with given attributes
+        """
+        self.data = data
+        self.next = next
 
 
-class Queue:
-  """
-  Class implementing Queue as a LinkedList
-  """
-  def __init__(self):
-    """
-    Initialises Queue object with pointers head and tail set to None
-    """
-    self.head = None
-    self.tail = None
+class LinkedList:
 
-  def enqueue(self, data) -> None:
-    """
-    Adds node containing data passed to the rear of the queue
-    """
-    new = Node(data)
-    if not self.tail is None:
-      self.tail.next = new
-    if self.head is None:
-      self.head = new
-    self.tail = new
+    def __init__(self):
+        """
+        Initialize the head
+        """
+        self.head = None
 
-  def dequeue(self) -> None:
-    """
-    Removes node from the rear of the queue
-    """
-    if not self.head is None:
-      self.head = self.head.next
-      if self.head is None:
-        self.tail = None
+    def insert_at_end(self, data):
+        new = Node(data, None)
+        current = self.head
+        if current is None:
+            self.head = new
+        else:
+            while current.next is not None:
+                current = current.next
+            current.next = new
 
-  def status(self) -> None:
-    """
-    It prints all the elements of Queue.
-    """
-    elements = ""
-    curr = self.head
-    while not curr is None:
-      elements += str(curr.data) + "=>"
-      curr = curr.next
-    print(elements + "None")
+    def status(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
 
 
-# Do not change the following code
-queue = Queue()
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = input_data.split(',')
-for i in range(len(operations)):
-  if operations[i] == "enqueue":
-    queue.enqueue(int(data[i]))
-  elif operations[i] == "dequeue":
-    queue.dequeue()
-queue.status()
+class Solution:
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
+        result = self.get_num(first_list) + self.get_num(second_list)
+        sum_list = LinkedList()
+        for digit in list(map(int, str(result)[::-1])):
+            sum_list.insert_at_end(digit)
+        return sum_list
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        curr = l.head
+        if curr is None:
+            return 0
+        num = ""
+        while curr is not None:
+            num = str(curr.data) + num
+            curr = curr.next
+        return int(num)
+
+# Do not edit the following code      
+# Create an instance for LinkedList
+first_list = LinkedList()
+# Create an another instance for LinkedList
+second_list = LinkedList()
+# Read data for first list
+data_for_first_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of first_list
+for data in data_for_first_list:
+    first_list.insert_at_end(data)
+# Read data for second list
+data_for_second_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of second_list
+for data in data_for_second_list:
+    second_list.insert_at_end(data)
+# Create an instance for Solution
+solution = Solution()
+# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
+new_list = solution.addTwoNumbers(first_list, second_list)
+# Display the status of new_list
+new_list.status()
